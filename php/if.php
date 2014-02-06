705692fe6b5e601938f664a5701343dabd9ac0b4
Ben Griffith <bengriffith@outlook.com>
2014-02-05 21:43:06 -0600

    refactor code to if/else and if/elseif/else

--- php/if.php
+++ php/if.php
@@ -1,48 +1,27 @@
 <?php
 
-$a = 5;
-$b = 10;
-$c = '10';
+$a = 22;
+$b = 14;
+$c = '36';
 
 if ($a < $b) {
-	// output the appropriate result
 	echo "$a is less than $b\n";
-}
-
-if ($b > $a) {
-	// output the appropriate result
-	echo "$b is greater than $a\n";
+} else {
+	echo "$a is greater than $b\n";
 }
 
 if ($b >= $c) {
-	// output the appropriate result
 	echo "$b is greater than or equal to $c\n";
-}
-if ($b <= $c) {
-	// output the appropriate result
+} else {
 	echo "$b is less than or equal to $c\n";
 }
 
-if ($b == $c) {
-	// output the appropriate result
-	echo "$b is equal to $c\n";
-}
-
 if ($b === $c) {
-	// output the appropriate result
 	echo "$b is identical to $c\n";
-}
-
-// output the appropriate result
-if ($b != $c) {
+} elseif ($b == $c) {
+	echo "$b is equal to $c\n";
+} else {
 	echo "$b is not equal to $c\n";
 }
-
-// output the appropriate result
-if ($b !== $c) {
-	echo "$b is not identical to $c\n";
-}
-
-
 
 ?>
