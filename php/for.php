3aeaff28723daccef42e34dc30577606991d46c3
Ben Griffith <bengriffith@outlook.com>
2014-02-06 14:11:28 -0600

    allowing user to select increment

--- php/for.php
+++ php/for.php
@@ -1,24 +1,19 @@
 <?php
 
-	fwrite(STDOUT, "What is the starting number?\n");
+	fwrite(STDOUT, "What is a number you would like to start with that is less than 10?\n");
 
 	$starting_number = fgets(STDIN);
 
-	fwrite(STDOUT, "What is the ending number?\n");
+	fwrite(STDOUT, "What is a number you would like to end with that is greater than 50?\n");
 
 	$ending_number = fgets(STDIN);
 
-	for ($i = $starting_number; $i <= $ending_number; $i += 1) {
+	fwrite(STDOUT, "Would you like to count by 1, 2, 4, 6, 8, or 10?\n");
+
+	$increment = fgets(STDIN);
+
+	for ($i = $starting_number; $i <= $ending_number; $i += $increment) {
 		fwrite(STDOUT, "/$i is equal to $i\n");
 	}
-
-
-
-
-
-
-
-
-
 
 ?>
