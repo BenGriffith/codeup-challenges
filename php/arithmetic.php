5c7a23cd423d819e3a03edb06c1d8e2e181340e3
Ben Griffith <bengriffith@outlook.com>
2014-02-09 21:43:28 -0600

    display an error if the input is not numeric

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,46 +1,65 @@
 <?php
 
-	
-
 	function add($a, $b) {
-		// Adding $a to $b 
-		echo $a + $b;
-		echo "\n";
+		// Adding $a to $b
+		if (is_numeric($a) && is_numeric($b)) {
+			echo $a + $b;
+			echo "\n";
+		} else {
+			echo "ERROR: Both arguments must be numbers\n";
+		}
 	}
+
+	add(3, 'test');
 
 	function subtract($a, $b) {
 		// Subtracting $b from $a
-		echo $a - $b;
-		echo "\n";
+		if (is_numeric($a) && is_numeric($b)) {
+			echo $a - $b;
+			echo "\n";
+		} else {
+			echo "ERROR: Both arguments must be numbers\n";
+		}
 	}
+
+	subtract(10, true);
 
 	function multiply($a, $b) {
 		// Multiplying $a and $b
-		echo $a * $b;
-		echo "\n";
+		if (is_numeric($a) && is_numeric($b)) {
+			echo $a * $b;
+			echo "\n";
+		} else {
+			echo "ERROR: Both arguments must be numbers\n";
+		}
 	}
+
+	multiply(array(5, 5, 'test'), 10);
 
 	function divide($a, $b) {
 		// Dividing $a by $b
-		echo $a / $b;
-		echo "\n";
+		if (is_numeric($a) && is_numeric($b)) {
+			echo $a / $b;
+			echo "\n";
+		} else {
+			echo "ERROR: Both arguments must be numbers\n";
+		}
 	}
 
-	add(4, 4);
-	subtract(10, 5);
-	multiply(5, 5);
-	divide(102, 5);
+	divide('test', 4);
 
 	function modulus($a, $b) {
-		// Modulus of $a
-		echo $a % 2;
-		echo "\n";
-
-		// Modulus of $b
-		echo $b % 2;
-		echo "\n";
+		// Modulus of $a and $b
+		if (is_numeric($a) && is_numeric($b)) {
+			echo $a % 2;
+			echo "\n";
+			echo $b % 2;
+			echo "\n";
+		} else {
+			echo "ERROR: Both arguments must be numbers\n";
+		}
 	}
 
-	modulus(22, 100);
+	modulus('test', 20);
 
 ?>
