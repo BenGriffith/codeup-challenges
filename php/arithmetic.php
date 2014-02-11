92742d08dc5e01af351317bcf2e068644a48d5e2
Ben Griffith <bengriffith@outlook.com>
2014-02-11 13:49:36 -0600

    refactor each function to return the result, removing the echo

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,73 +1,70 @@
 <?php
 
-	function error($a, $b) {
+	function error_message($a, $b) {
 		echo "ERROR: Arguments must be numbers and greater than 0 instead of {$a} or {$b}\n";
 	}
 
 	function add($a, $b) {
 		// Adding $a to $b
 		if (is_numeric($a) && is_numeric($b)) {
-			echo $a + $b;
-			echo "\n";
+			return $a + $b;
 		} else {
-			error($a, $b);
+			error_message($a, $b);
 		}
 	}
 
- 	add('test', 4);
 
 	function subtract($a, $b) {
 		// Subtracting $b from $a
 		if (is_numeric($a) && is_numeric($b)) {
-			echo $a - $b;
-			echo "\n";
+			return $a - $b;
 		} else {
-			error($a, $b); 
+			error_message($a, $b); 
 		}
 	}
 
-	subtract(10, 2);
 
 	function multiply($a, $b) {
 		// Multiplying $a and $b
 		if (is_numeric($a) && is_numeric($b)) {
-			echo $a * $b;
-			echo "\n";
+			return $a * $b;
 		} else {
-			error($a, $b);
+			error_message($a, $b);
 		}
 	}
 
-	multiply(10, 10);
 
 	function divide($a, $b) {
 		// Dividing $a by $b
 		if (is_numeric($a) && is_numeric($b)) {
-			echo $a / $b;
-			echo "\n";
-		} else {
-			error($a, $b);
-		}
-
-		if ($b == 0) {
-			error($a, $b);
+			return $a / $b;
+			if ($b == 0) {
+				error_message($a, $b);
+			}
 		}
 	}
 
-	divide(10, 10);
 
 	function modulus($a, $b) {
 		// Modulus of $a and $b
 		if (is_numeric($a) && is_numeric($b)) {
-			echo $a % 2;
-			echo "\n";
-			echo $b % 2;
-			echo "\n";
+			return $a % 2;
+			return $b % 2;
 		} else {
-			error($a, $b);
+			error_message($a, $b);
 		}
 	}
 
-	modulus(10, 10);
+ 	$addition = add(3, 4);
+	$subtraction = subtract(10, 2);
+	$multiplication = multiply(10, 10);
+	$division = divide(10, 0);
+	$mod = modulus(10, 10);
+
+	echo $addition . "\n";
+	echo $subtraction . "\n";
+	echo $multiplication . "\n";
+	echo $division . "\n";
+	echo $mod . "\n";
 
 ?>
