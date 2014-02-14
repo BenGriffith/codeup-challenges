0805763bfb20bc8163a47230483038a014e7fdc4
Ben Griffith <bengriffith@outlook.com>
2014-02-14 17:54:46 -0600

    sort physicists by first name

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,7 +1,7 @@
 <?php
 
 	function humanized_list($string) {
-		// changing $physicists_string into an array
+		// converting $physicists_string into an array
 		$physicists_array = explode(', ', $string);
 
 		// removing last item in $physicists_array
@@ -10,8 +10,13 @@
 		// adding item to the end of $physicists_array
 		array_push($physicists_array, 'and Tony Stark');
 
+		// sort the physicists by first name
+		asort($physicists_array);
+
+		// converting $physicists_array into a string
 		$implosion = implode(', ', $physicists_array);
 
+		// return value of $implosion
 		return $implosion;
 	}
 
@@ -24,16 +29,5 @@
 
 	// outputting to the screen
 	echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}." . PHP_EOL;
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
